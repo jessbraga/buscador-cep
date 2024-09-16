9fa6d3345fe1032c16f04bd7ebda35b72554da07
Jéssica Braga <bragamarquesjessica@gmail.com>
2024-09-16 19:26:13 -0300

    primeiro commit

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,10 +1,6 @@
 import { AlertCircle } from "lucide-react"
 
-import {
-  Alert,
-  AlertDescription,
-  AlertTitle,
-} from "@/components/ui/alert"
+import {Alert,AlertDescription,AlertTitle} from "@/components/ui/alert"
 
 export function AlertError() {
   return (
